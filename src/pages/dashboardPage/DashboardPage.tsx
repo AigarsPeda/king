733e130cc51ac45e7443fc73a522486cbe987681
Aigars Peda <aigarspeda@gmail.com>
2021-03-14 22:55:02 +0200

    Update DashboardPage.tsx

--- src/pages/dashboardPage/DashboardPage.tsx
+++ src/pages/dashboardPage/DashboardPage.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getAllGames } from "../../redux/actions/gamesActions";
 
 const DashboardPage: React.FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllGames());
+  }, [dispatch]);
   return (
     <div>
       <h1>Stats And Game Data</h1>
