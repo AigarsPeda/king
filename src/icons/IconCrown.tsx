74b44e5a8c2b1814c79157efd72dd1d5663ca19f
Aigars Peda <aigarspeda@gmail.com>
2021-03-19 06:21:23 +0200

    Update IconCrown.tsx

--- src/icons/IconCrown.tsx
+++ src/icons/IconCrown.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const IconCrown = (props: React.SVGProps<SVGSVGElement>): JSX.Element => {
   return (
     <svg
-      viewBox="0 0 64 54"
+      viewBox="0 0 65 54"
       xmlns="http://www.w3.org/2000/svg"
       fillRule="evenodd"
       clipRule="evenodd"
@@ -12,7 +12,7 @@ const IconCrown = (props: React.SVGProps<SVGSVGElement>): JSX.Element => {
       {...props}
     >
       <path
-        d="M8 37.333L0 10.667l18.944 10.666L32 0l13.056 21.333L64 10.667l-8 26.666H8zm0 5.334v10.666h48V42.667H8z"
+        d="M2.33 37.71L0 10.666l18.944 10.666L32 0l13.056 21.333L64 10.667l-2.152 26.666-59.517.376zm.06 4.957v10.666l59.549-.197V42.47l-59.55.198z"
         fillRule="nonzero"
       />
     </svg>
