e3e4e38c9c77a44a08a3cf9829272e16f0dc5305
Aigars Peda <aigarspeda@gmail.com>
2021-03-21 07:20:43 +0200

    Update DashboardPage.tsx

--- src/pages/dashboardPage/DashboardPage.tsx
+++ src/pages/dashboardPage/DashboardPage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import Profile from "../../components/Profile/Profile";
 import { getAllGames } from "../../redux/actions/gamesActions";
 
 const DashboardPage: React.FC = () => {
@@ -9,13 +10,18 @@ const DashboardPage: React.FC = () => {
     dispatch(getAllGames());
   }, [dispatch]);
   return (
-    <div>
-      <h1>Stats And Game Data!</h1>
-      <h2>TODO: What sport do you play!</h2>
-      <p>
-        There shoed be possibility to chose more then one sport. End score to
-        determine how winner is selected should change.
-      </p>
+    <div className="dashboard">
+      <section className="dashboard-profile">
+        <Profile />
+      </section>
+      <section className="dashboard-main">
+        <h1>Stats And Game Data!</h1>
+        <h2>TODO: What sport do you play!</h2>
+        <p>
+          There shoed be possibility to chose more then one sport. End score to
+          determine how winner is selected should change.
+        </p>
+      </section>
     </div>
   );
 };
