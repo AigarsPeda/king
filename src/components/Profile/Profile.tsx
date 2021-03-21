85785f0b8700c70bac8f5f6cfa314b7baf441045
Aigars Peda <aigarspeda@gmail.com>
2021-03-21 07:20:29 +0200

    Update Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import CloseIcon from "../../icons/CloseIcon";
 import CrownIcon from "../../icons/CrownIcon";
 import GearIcon from "../../icons/GearIcon";
 import MenuIcon from "../../icons/MenuIcon";
+import ProfileIcon from "../../icons/ProfileIcon";
 
 const Profile: React.FC = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleMenuOpenClose = () => {
+    setToggle((state) => (state = !state));
+  };
+
   return (
     <div className="profile">
       <div className="profile-header">
-        <MenuIcon className="profile-menu-icon" />
+        <MenuIcon className="profile-menu-icon" onClick={handleMenuOpenClose} />
         <CrownIcon className="profile-crown-icon" />
         <GearIcon className="profile-gear-icon" />
+      </div>
+      <div className="profile-info">
+        <div className="profile-profile-icon-container">
+          <ProfileIcon className="profile-profile-icon" />
+        </div>
+      </div>
+      <div
+        className={`profile-menu-modal ${toggle ? "toggleIn" : "toggleOut"}`}
+      >
+        <div className="profile-menu">
+          <div className="profile-close">
+            <CloseIcon
+              className="profile-close-icon"
+              onClick={handleMenuOpenClose}
+            />
+          </div>
+          <p>Dashboard</p>
+          <p>My Feed</p>
+          <p>Hot Tips</p>
+        </div>
       </div>
       <div>Profile Sections</div>
     </div>
