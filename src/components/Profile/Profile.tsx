b76251c6134266c17a8afea1d0c12348d4cc77a3
Aigars Peda <aigarspeda@gmail.com>
2021-03-23 07:25:45 +0200

    Update Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,27 +1,70 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import CloseIcon from "../../icons/CloseIcon";
 import CrownIcon from "../../icons/CrownIcon";
-import GearIcon from "../../icons/GearIcon";
+import MarkIcon from "../../icons/MarkIcon";
 import MenuIcon from "../../icons/MenuIcon";
-import ProfileIcon from "../../icons/ProfileIcon";
+import profile from "../../images/prof.jpeg";
+import { getUserStates } from "../../redux/actions/userActions";
+import { RootStateType } from "../../redux/reducers/reducers";
+import MyStats from "../MyStats/MyStats";
 
 const Profile: React.FC = () => {
+  const dispatch = useDispatch();
+  const [selectedInfo, setSelectedInfo] = useState<
+    "dashboard" | "mystats" | "hottips"
+  >("dashboard");
   const [toggle, setToggle] = useState(false);
+
+  const { tournamentsWon, tournamentsLost } = useSelector(
+    (state: RootStateType) => ({
+      tournamentsWon: state.user.states.tournaments_won,
+      tournamentsLost: state.user.states.tournaments_lost
+    })
+  );
 
   const handleMenuOpenClose = () => {
     setToggle((state) => (state = !state));
   };
 
+  const handleSelectedInfo = (str: "dashboard" | "mystats" | "hottips") => {
+    setSelectedInfo(str);
+
+    // to close profile-menu-modal when mobile
+    setToggle(false);
+  };
+
+  useEffect(() => {
+    dispatch(getUserStates());
+  }, [dispatch]);
+
   return (
     <div className="profile">
       <div className="profile-header">
         <MenuIcon className="profile-menu-icon" onClick={handleMenuOpenClose} />
-        <CrownIcon className="profile-crown-icon" />
-        <GearIcon className="profile-gear-icon" />
       </div>
       <div className="profile-info">
-        <div className="profile-profile-icon-container">
-          <ProfileIcon className="profile-profile-icon" />
+        <CrownIcon className="profile-crown-icon" />
+        <div className="profile-profile-images-container">
+          <img src={profile} alt="profile owners" />
+        </div>
+        <div className="profile-stats">
+          <div className="profile-won">
+            <div>
+              <h3>{tournamentsWon}</h3>
+              <p>
+                <MarkIcon className="profile-mark-icon" /> Won
+              </p>
+            </div>
+          </div>
+          <div className="profile-lost">
+            <div>
+              <h3>{tournamentsLost}</h3>
+              <p>
+                <CloseIcon className="profile-close-icon" /> Lost
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <div
@@ -34,12 +77,29 @@ const Profile: React.FC = () => {
               onClick={handleMenuOpenClose}
             />
           </div>
-          <p>Dashboard</p>
-          <p>My Feed</p>
-          <p>Hot Tips</p>
+          <button
+            className={selectedInfo === "dashboard" ? "selected" : ""}
+            onClick={() => handleSelectedInfo("dashboard")}
+          >
+            Dashboard
+          </button>
+          <button
+            className={selectedInfo === "mystats" ? "selected" : ""}
+            onClick={() => handleSelectedInfo("mystats")}
+          >
+            My Stats
+          </button>
+          <button
+            className={selectedInfo === "hottips" ? "selected" : ""}
+            onClick={() => handleSelectedInfo("hottips")}
+          >
+            Hot Tips
+          </button>
         </div>
       </div>
-      <div>Profile Sections</div>
+      {selectedInfo === "dashboard" && <div>Dashboard</div>}
+      {selectedInfo === "mystats" && <MyStats />}
+      {selectedInfo === "hottips" && <div>Hot Tips</div>}
     </div>
   );
 };
