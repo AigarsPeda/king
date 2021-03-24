b3cc7d268a1dacbcb522232511e95f6d32286b6b
Aigars Peda <aigarspeda@gmail.com>
2021-03-24 06:26:40 +0200

    Create uiReducer.ts

---
+++ src/redux/reducers/uiReducer.ts
@@ -0,0 +1,29 @@
+import { IProfileSelectedInfo } from "../../types";
+import { UIActionTypes, SET_PROFILE_SELECTED_INFO } from "../types/uiTypes";
+
+export interface IAuthState {
+  selectedInfo: IProfileSelectedInfo;
+}
+
+// Initial State
+const initialState: IAuthState = {
+  selectedInfo: "dashboard"
+};
+
+export const UIReducer = (
+  state = initialState,
+  action: UIActionTypes
+): IAuthState => {
+  switch (action.type) {
+    case SET_PROFILE_SELECTED_INFO:
+      return {
+        ...state,
+        selectedInfo: action.payload
+      };
+    // case UNAUTHENTICATED_USER:
+    //   return initialState;
+
+    default:
+      return state;
+  }
+};
