b25c0c60f28c4c0ccbff5f49bc7b1d155cb88d4f
Aigars Peda <aigarspeda@gmail.com>
2021-03-25 07:05:02 +0200

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,4 @@
-import { useMemo } from "react";
-import { createStore, applyMiddleware } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunkMiddleware from "redux-thunk";
 import reducers from "./reducers/reducers";
