f102766791811ae976d8c7337ff01b425afd96ad
Aigars Peda <aigarspeda@gmail.com>
2021-03-25 07:05:10 +0200

    Update userReducer.ts

--- src/redux/reducers/userReducer.ts
+++ src/redux/reducers/userReducer.ts
@@ -1,12 +1,14 @@
-import { IUser } from "../../types";
+import { IUser, IUserStates } from "../../types";
 import {
   UserActionTypes,
   SET_USER_DATA,
-  CLEAR_USER_DATA
+  CLEAR_USER_DATA,
+  SET_USER_STATES
 } from "../types/userTypes";
 
 export interface IUserState {
   user: IUser;
+  states: IUserStates;
 }
 
 // Initial State
@@ -16,6 +18,15 @@ const initialState: IUserState = {
     email: "",
     name: "",
     surname: "",
+    user_id: 0
+  },
+  states: {
+    stats_id: 0,
+    points_overall: 0,
+    tournaments_played: 0,
+    tournaments_won: 0,
+    tournaments_lost: 0,
+    tournaments_created: 0,
     user_id: 0
   }
 };
@@ -29,6 +40,12 @@ export const userReducer = (
       return {
         ...state,
         user: action.payload
+      };
+
+    case SET_USER_STATES:
+      return {
+        ...state,
+        states: action.payload
       };
 
     case CLEAR_USER_DATA:
