6a38977dfa94068ef2ea1dc65feccee54ed621c4
Aigars Peda <aigarspeda@gmail.com>
2021-03-26 22:00:13 +0200

    Update DashboardPage.tsx

--- src/pages/dashboardPage/DashboardPage.tsx
+++ src/pages/dashboardPage/DashboardPage.tsx
@@ -1,26 +1,47 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Profile from "../../components/Profile/Profile";
 import { getAllGames } from "../../redux/actions/gamesActions";
+import { RootStateType } from "../../redux/reducers/reducers";
 
 const DashboardPage: React.FC = () => {
   const dispatch = useDispatch();
 
+  const { games } = useSelector((state: RootStateType) => ({
+    games: state.games.games
+  }));
+
+  /** Fetch all user stats only when
+   * stats component are displayed * */
+
+  /** All user games are fetch in authActions
+   * when
+   * log in or sign up
+   * because they are needed at the start * */
+
   useEffect(() => {
     dispatch(getAllGames());
   }, [dispatch]);
+
   return (
     <div className="dashboard">
+      {console.log(games)}
       <section className="dashboard-profile">
         <Profile />
       </section>
       <section className="dashboard-main">
-        <h1>Stats And Game Data!</h1>
+        <h2>TODO: Add second password input!</h2>
+        <p>Make sure the passwords are the same</p>
+        <h2>TODO: Loading state for login and sing up!</h2>
+        <p>Add loading display on button</p>
+        <h2>TODO: Adding game!</h2>
+        <h2>TODO: Keeping score!</h2>
         <h2>TODO: What sport do you play!</h2>
         <p>
           There shoed be possibility to chose more then one sport. End score to
           determine how winner is selected should change.
         </p>
+        <h2>TODO: Edit user data!</h2>
       </section>
     </div>
   );
