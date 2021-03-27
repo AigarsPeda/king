a07653b723a6fa063327be13cfcc9e41f2d2a450
Aigars Peda <aigarspeda@gmail.com>
2021-03-27 10:01:35 +0200

    Update userTypes.ts

--- src/redux/types/userTypes.ts
+++ src/redux/types/userTypes.ts
@@ -1,6 +1,5 @@
 import { IUser, IUserStates } from "../../types";
 
-// USER TYPES
 export const SET_USER_DATA = "SET_USER_DATA";
 interface ISetUserAction {
   type: typeof SET_USER_DATA;
@@ -10,13 +9,11 @@ interface ISetUserAction {
 export const SET_USER_DATA_LOADING = "SET_USER_DATA_LOADING";
 interface ISetUserDataLoadingAction {
   type: typeof SET_USER_DATA_LOADING;
-  // payload: boolean;
 }
 
 export const SET_USER_DATA_NOT_LOADING = "SET_USER_DATA_NOT_LOADING";
 interface ISetUserDataNotLoadingAction {
   type: typeof SET_USER_DATA_NOT_LOADING;
-  // payload: boolean;
 }
 
 export const SET_USER_STATES = "SET_USER_STATES";
@@ -28,20 +25,12 @@ interface ISetUserStatesAction {
 export const SET_USER_STATES_LOADING = "SET_USER_STATES_LOADING";
 interface ISetUserStatesLoadingAction {
   type: typeof SET_USER_STATES_LOADING;
-  // payload: boolean;
 }
 
 export const SET_USER_STATES_NOT_LOADING = "SET_USER_STATES_NOT_LOADING";
 interface ISetUserStatesNotLoadingAction {
   type: typeof SET_USER_STATES_NOT_LOADING;
-  // payload: boolean;
 }
-
-// export const GET_USER_DATA = "GET_USER_DATA";
-// interface IGetUserDataAction {
-//   type: typeof GET_USER_DATA;
-//   payload: IUser;
-// }
 
 export const CLEAR_USER_DATA = "CLEAR_USER_DATA";
 interface IClearUserDataAction {
