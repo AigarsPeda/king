84244f0976a5f47d12e19887d813a9a246fb8623
Aigars Peda <aigarspeda@gmail.com>
2021-04-06 07:07:47 +0300

    Update validateCreateUser.ts

--- src/helpers/validateCreateUser.ts
+++ src/helpers/validateCreateUser.ts
@@ -1,3 +1,5 @@
+import { emailPattern } from "../utils/emailRegExp";
+
 export const validateCreateUser = ({
   name,
   surname,
@@ -14,10 +16,6 @@ export const validateCreateUser = ({
   isValid: boolean;
   errorMessage: string;
 } => {
-  const emailPattern = new RegExp(
-    /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
-  );
-
   let isValid = true;
   let errorMessage = "";
 
