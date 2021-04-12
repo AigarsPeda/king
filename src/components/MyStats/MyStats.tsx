7f15ded4d5fdc9f75299df8bd385ef21d329da9a
Aigars Peda <aigarspeda@gmail.com>
2021-04-12 14:37:20 +0300

    Update MyStats.tsx

--- src/components/MyStats/MyStats.tsx
+++ src/components/MyStats/MyStats.tsx
@@ -19,29 +19,34 @@ const MyStats: React.FC = () => {
 
   return (
     <div className="my-stats">
-      <Doughnut
-        data={{
-          labels: ["win", "lost"],
-          datasets: [
-            {
-              data: [tournamentsWon, tournamentsLost],
-              backgroundColor: [
-                "rgb(54, 162, 235)",
-                "rgb(255, 99, 132)",
-                "rgb(255, 205, 86)"
-              ]
-            }
-          ],
-          hoverOffset: 4
-        }}
-        options={{
-          responsive: true,
-          maintainAspectRatio: true
-        }}
-        width={100}
-        height={75}
-      />
-      <h1>My States</h1>
+      {tournamentsWon <= 0 || tournamentsLost <= 0 ? (
+        <div className="my-stats-empty">
+          <h3>No stats yet</h3>
+        </div>
+      ) : (
+        <Doughnut
+          data={{
+            labels: ["win", "lost"],
+            datasets: [
+              {
+                data: [tournamentsWon, tournamentsLost],
+                backgroundColor: [
+                  "rgb(54, 162, 235)",
+                  "rgb(255, 99, 132)",
+                  "rgb(255, 205, 86)"
+                ]
+              }
+            ],
+            hoverOffset: 4
+          }}
+          options={{
+            responsive: true,
+            maintainAspectRatio: true
+          }}
+          width={100}
+          height={75}
+        />
+      )}
     </div>
   );
 };
