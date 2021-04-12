142f37d0c0d03a8bcb9564e2a5496327f9fa776a
Aigars Peda <aigarspeda@gmail.com>
2021-04-12 14:37:13 +0300

    Create ShareIcon.tsx

---
+++ src/icons/ShareIcon.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const ShareIcon = (props: React.SVGProps<SVGSVGElement>): JSX.Element => {
+  return (
+    <svg width={24} height={24} viewBox="0 0 24 24" {...props}>
+      <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10S2 17.514 2 12 6.486 2 12 2zm0-2C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zM6 17c1.513-6.587 7-7.778 7-7.778V7l5 4.425-5 4.464v-2.223c0 .001-3.78-.114-7 3.334z" />
+    </svg>
+  );
+};
+
+export default ShareIcon;
