76772470c23e3e35c2c5117bfcc6eeb24d946049
Aigars Peda <aigarspeda@gmail.com>
2021-04-30 06:25:53 +0300

    Delete gamesTypes.ts

--- src/redux/types/gamesTypes.ts
+++
@@ -1,9 +0,0 @@
-import { IGame } from "../../types";
-
-export const SET_USERS_GAMES = "SET_USERS_GAMES";
-interface ISetUserGamesAction {
-  type: typeof SET_USERS_GAMES;
-  payload: IGame[];
-}
-
-export type GamesActionTypes = ISetUserGamesAction;
