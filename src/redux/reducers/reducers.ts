14396ce4cb4f523f866155417a3bb6bfe2bf02cc
Aigars Peda <aigarspeda@gmail.com>
2021-05-03 08:18:50 +0300

    Update reducers.ts

--- src/redux/reducers/reducers.ts
+++ src/redux/reducers/reducers.ts
@@ -3,13 +3,13 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { authReducer } from "./authReducer";
 import { errorsReducer } from "./errorsReducer";
-import { gamesReducer } from "./gamesReducer";
+import { tournamentsReducer } from "./tournamentsReducer";
 import { UIReducer } from "./uiReducer";
 import { userReducer } from "./userReducer";
 
-// COMBINED REDUCERS
+/* COMBINED REDUCERS **/
 const reducers = {
-  games: gamesReducer,
+  tournaments: tournamentsReducer,
   errors: errorsReducer,
   auth: authReducer,
   user: userReducer,
@@ -18,10 +18,11 @@ const reducers = {
 
 const appReducers = combineReducers(reducers);
 
+/* WHITE LISTS REDUCER TO STORE STATE BETWEEN BROWSER REFRESHES  **/
 const persistConfig = {
   key: "primary",
   storage,
-  whitelist: ["auth", "user", "ui"] // place to select which state you want to persist
+  whitelist: ["auth", "user", "ui", "tournaments"]
 };
 
 export type RootStateType = ReturnType<typeof appReducers>;
