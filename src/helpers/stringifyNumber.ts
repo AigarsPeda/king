29e0746ddec2b069903ea26f7844b2e2cfdff6f6
Aigars Peda <aigarspeda@gmail.com>
2021-05-07 04:35:06 +0300

    Create stringifyNumber.ts

---
+++ src/helpers/stringifyNumber.ts
@@ -0,0 +1,26 @@
+const special = [
+  "Zeroth",
+  "First",
+  "Second",
+  "Third",
+  "Fourth",
+  "Fifth",
+  "Sixth",
+  "Seventh",
+  "Eighth",
+  "Ninth",
+  "Tenth",
+  "Eleventh",
+  "Twelfth",
+  "Thirteenth",
+  "Fourteenth",
+  "Fifteenth",
+  "Sixteenth",
+  "Seventeenth",
+  "Eighteenth",
+  "Nineteenth"
+];
+
+export const stringifyNumber = (n: number): string => {
+  return special[n];
+};
